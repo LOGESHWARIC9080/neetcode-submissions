ac776a54f539326a390f6b18a2c65e6c72750207
LOGESHWARIC9080 <146337089+LOGESHWARIC9080@users.noreply.github.com>
2026-08-24 11:04:47 +0530

    Add: sql-customers-bought-a-b-not-c - submission-0

---
+++ SQL/sql-customers-bought-a-b-not-c/submission-0.sql
@@ -0,0 +1,22 @@
+-- Write your query below
+select c.customer_id,c.customer_name
+from customers c
+where exists(
+    select 1
+    from orders o
+    where c.customer_id=o.customer_id
+    and o.product_name='A'
+)
+and exists(
+    select 1 
+    from orders o
+    where c.customer_id=o.customer_id
+    and o.product_name='B'
+)
+and not exists(
+    select 1 
+    from orders o
+    where c.customer_id=o.customer_id
+    and o.product_name='C'
+)
+order by c.customer_name;
